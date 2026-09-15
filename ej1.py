b497c93a6a81075f687659ddee3a3e8a4b9b2e90
Celia <senamartinezcelia@icloud.com>
2026-09-15 13:04:07 +0200

    ej1

---
+++ ej1.py
@@ -0,0 +1,5 @@
+nombre: str = "Celia"
+nivel: int = 2
+vida: float = 12.5
+esta_vivo: bool = True
+print(nombre, nivel, vida, esta_vivo)
---
+++ ej3.py
@@ -0,0 +1,4 @@
+edad: int = 1
+nombre: str = "Ana"
+tiene_tomates: bool = True
+print(edad, nombre, tiene_tomates)
